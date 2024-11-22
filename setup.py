212a37a0bc2a3419845421649cea55bf2d7e4f1a
Utkarsh Wagh <you@example.com>
2024-11-22 14:00:12 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='utkarsh',
+name='mlproject',
 version='0.0.1',
 author='utkarsh',
 author_email='utkarsh12wagh@gmail.com',
